296122810e6e7beec1c3c7a3b7e0548e80de4f89
Shaurya Priyadarshi <shpr24680@gmail.com>
2023-06-24 19:03:47 +0530

    debugged the error in shell_pwd

--- shell_pwd.c
+++ shell_pwd.c
@@ -1,7 +1,7 @@
 int shell_pwd(char **arguments)
 {
     char *temp=malloc(100 * sizeof(char));
-    getcwd(temp, 100 * sizeof(char))
+    getcwd(temp, 100 * sizeof(char));
     printf("%s\n", temp);
     return 1;
 }
